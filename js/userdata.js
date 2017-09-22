f444d879981cd22db1014b6bd3a4865740c6622e
yuxuan39 <kumo9102151@gmail.com>
2017-09-22 14:30:19 +0800

    getData functon is done

--- js/userdata.js
+++ js/userdata.js
@@ -1,10 +1,24 @@
 const token = sessionStorage.getItem('userdata')
+let url = new URL("http://free-gce.akiya.com.tw:4000/users/me")
+url.searchParams.append('access_token', token)
+console.log(url.href)
 
-function parseJwt(token) {
-    var base64Url = token.split('.')[1];
-    var base64 = base64Url.replace('-', '+').replace('_', '/');
-    return JSON.parse(window.atob(base64));
-};
 
-parseJwt()
-// console.log(userdata)
+const getData = () => {
+    fetch(url, {
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        },
+    }).then(res => {
+        alert('get Completed')
+        return res.json()
+    }).then(data => {
+        console.log(data)
+    }).catch(err => {
+        console.log(err)
+        alert('Oops!')
+    })
+}
+getData()
