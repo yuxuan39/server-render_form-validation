407811ae531c3d2854d76e2bb0f78070e50ad378
yuxuan39 <kumo9102151@gmail.com>
2017-09-22 15:28:24 +0800

    render functon is done

--- js/userdata.js
+++ js/userdata.js
@@ -1,3 +1,9 @@
+let userId = document.getElementById('user-id')
+let userName = document.getElementById('user-username')
+let userEmail = document.getElementById('user-email')
+let userPhone = document.getElementById('user-phone')
+let userBirth = document.getElementById('user-birth')
+
 const token = sessionStorage.getItem('userdata')
 let url = new URL("http://free-gce.akiya.com.tw:4000/users/me")
 url.searchParams.append('access_token', token)
@@ -12,13 +18,24 @@ const getData = () => {
             'Access-Control-Allow-Origin': '*'
         },
     }).then(res => {
-        alert('get Completed')
+        alert('got it!')
         return res.json()
     }).then(data => {
         console.log(data)
+        const id = data.data['_id']
+        const username = data.data['username']
+        const email = data.data['email']
+        const phone = data.data['phone']
+        const birth = data.data['birthday']
+        userId.innerText = id
+        userName.innerText = username
+        userEmail.innerText = email
+        userPhone.innerText = phone
+        userBirth.innerText = birth
     }).catch(err => {
         console.log(err)
         alert('Oops!')
     })
 }
 getData()
+
